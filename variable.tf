26c94f269cda46a85e0bf7cd4c611c4f8605dd20
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-07 22:09:11 +0100

    Removed outputs.tf provider.tf and variable.tf 'cause I have moved those files to modules/ec2_instance

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_value" {
-  description = "The AMI to use for the instance"
-  type        = string
-}
-
-variable "instance_type_value" {
-  description = "The type of instance to use"
-  type        = string
-}
