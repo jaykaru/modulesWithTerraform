e6fdbeee30e726a18ea8601e05e7cf95d594e484
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-07 22:02:23 +0100

    aws ec2 module added to create ec2 from aws module

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
+module "ec2_instance" {
+  source              = "./modules/ec2_instance"
+  ami_value           = "ami-046c2381f11878233"
+  instance_type_value = "t3.micro"
+}
 
-resource "aws_instance" "example" {
-  ami           = var.ami_value
-  instance_type = var.instance_type_value
+module "ec2-instance" {
+  source        = "terraform-aws-modules/ec2-instance/aws"
+  version       = "6.1.1"
+  instance_type = "t3.micro"
+  ami           = "ami-046c2381f11878233"
+  subnet_id = "subnet-037fc7309c3dcd018"
+  name = "my-instance"
+
 }
