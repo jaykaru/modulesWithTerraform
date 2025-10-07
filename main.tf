3a66c3fef5464d2a15f87d2b92d0248943cd3ba3
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-07 22:49:40 +0100

    s3 bucket module added

--- main.tf
+++ main.tf
@@ -4,12 +4,17 @@ module "ec2_instance" {
   instance_type_value = "t3.micro"
 }
 
-module "ec2-instance" {
-  source        = "terraform-aws-modules/ec2-instance/aws"
-  version       = "6.1.1"
-  instance_type = "t3.micro"
-  ami           = "ami-046c2381f11878233"
-  subnet_id = "subnet-037fc7309c3dcd018"
-  name = "my-instance"
+# module "ec2-instance" {
+#   source        = "terraform-aws-modules/ec2-instance/aws"
+#   version       = "6.1.1"
+#   instance_type = "t3.micro"
+#   ami           = "ami-046c2381f11878233"
+#   subnet_id = "subnet-037fc7309c3dcd018"
+#   name = "my-instance"
 
+# }
+
+module "s3_bucket" {
+  source = "./modules/s3_bucket"
+  state_bucket = "jk-terraform-state-bucket-12345"
 }
