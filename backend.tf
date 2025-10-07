855c629a5aa3ec47e15994b1fa3490d8a344245c
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-08 00:29:09 +0100

    backen.tf move to root module

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "jk-terraform-state-bucket-12345"
-#     key    = "jkstatefile/terraform.tfstate"
-#     region = "eu-west-2"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "jk-terraform-state-bucket-12345"
+    key    = "jkstatefile/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
 
