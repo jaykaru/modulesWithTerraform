4e5f5ef5ecd4fd5d9612b58b6e7731ae1bb8f74c
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-08 13:31:38 +0100

    commented backend code

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "jk-terraform-state-bucket-12345"
-    key    = "jkstatefile/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "jk-terraform-state-bucket-12345"
+#     key    = "jkstatefile/terraform.tfstate"
+#     region = "eu-west-2"
+#     # dynamodb_table = "terraform-lock-table"
+#   }
+# }
 
