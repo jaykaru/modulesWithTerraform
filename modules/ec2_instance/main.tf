1fc5c88c8c22b6c3b92119458da575cd73b83b28
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-13 19:05:19 +0100

    provisioner source path updated

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "web_server" {
 
 // File provisioner to copy a file from local to remote EC2 instance
   provisioner "file" {
-    source = "${path.module}../../scripts/app.py"  //${path.module} ensures the path is resolved relative to the module, not the root working directory
+    source = ("${path.module}/../../scripts/app.py")  //${path.module} ensures the path is resolved relative to the module, not the root working directory
     destination = "/home/ubuntu/app.py" // path on the remote instance
     
   }
