4cd924cf3d5c5c0abda566980bed5dfed3be0171
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-10-13 19:06:31 +0100

    app.py

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def main():
-    print("Hello, welcome to AI world")
+    return "Hello, welcome to AI world"
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=80)
