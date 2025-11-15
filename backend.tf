36adc47bf77efe0d87bbab0fae4e5968d4d80d83
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-11-15 15:35:42 +0000

    hostname is added so terraform knows which token to use

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
+    hostname = "app.terraform.io"
     organization = "JayKaruProject"
-
     workspaces {
       prefix = "env-" # Workspaces will be env-dev, env-prod, etc.
     }
