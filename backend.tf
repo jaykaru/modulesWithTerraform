1f3c1f65a66034cd98be1fec09fa286341e83eb9
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-11-15 15:44:33 +0000

    formatted

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "JayKaruProject"
     workspaces {
       prefix = "env-" # Workspaces will be env-dev, env-prod, etc.
