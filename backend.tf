94741e7222d50b3a68803a981f1657138d6c67c8
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-11-17 16:11:26 +0000

    updated workspace

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "JayKaruProject" # Must match exactly
   workspaces {
-      name = "" # Placeholder, will be overridden by TF_WORKSPACE
+      name = "dev" 
     }
   }
 }
