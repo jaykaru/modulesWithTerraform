466a15904e6185e6adc25611c8df80f2a0164eb5
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-11-17 15:21:53 +0000

    added workspaces

--- backend.tf
+++ backend.tf
@@ -2,5 +2,8 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "JayKaruProject" # Must match exactly
+  workspaces {
+      name = "dev" # Placeholder, will be overridden by TF_WORKSPACE
+    }
   }
 }
