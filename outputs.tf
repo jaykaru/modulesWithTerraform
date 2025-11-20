7b314d0120d45ebbf41e9e04a79f342ed3346ad0
karuppiah.jay <karuppiah.jay@royalmail.onmicrosoft.com>
2025-11-20 16:03:28 +0000

    added outputs for instance id and public ip

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+  value       = aws_instance.web_server.id
+  description = "ID of the EC2 instance"
+}
+
+output "public_ip" {
+  value       = aws_instance.web_server.public_ip
+  description = "Public IP of the EC2 instance"
+}
+
+# output "private_ip" {
+#   value       = aws_instance.web_server.private_ip
+#   description = "Private IP of the EC2 instance"
+# }
